196c60887e9283b60cae248acad11c61ce3c56ab
Ivo Pačnik <pacnik@gmail.com>
2024-11-26 18:48:04 +0100

    button can shift pattern

--- src/main.c
+++ src/main.c
@@ -9,11 +9,14 @@
 #include <zephyr/drivers/gpio.h>
 
 /* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   200
+#define SLEEP_TIME_MS   500
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
 #define LED0_NODE2 DT_ALIAS(led1)
+
+
+#define SW0_NODE	DT_ALIAS(sw0)
 
 /*
  * A build error on this line means your board is unsupported.
@@ -21,6 +24,18 @@
  */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED0_NODE2, gpios);
+
+
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios,
+							      {0});
+static struct gpio_callback button_cb_data;
+
+
+void button_pressed(const struct device *dev, struct gpio_callback *cb,
+		    uint32_t pins)
+{
+	printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
+}
 
 int main(void)
 {
@@ -36,12 +51,41 @@ int main(void)
 		return 0;
 	}
 
+	if (!gpio_is_ready_dt(&button)) {
+		printk("Error: button device %s is not ready\n",
+		       button.port->name);
+		return 0;
+	}
+
 
 	ret = gpio_pin_configure_dt(&led2, GPIO_OUTPUT_ACTIVE);
 	if (ret < 0) {
 		return 0;
 	}
-	
+
+
+	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+	if (ret != 0) {
+		printk("Error %d: failed to configure %s pin %d\n",
+		       ret, button.port->name, button.pin);
+		return 0;
+	}
+
+
+	ret = gpio_pin_interrupt_configure_dt(&button,
+					      GPIO_INT_EDGE_TO_ACTIVE);
+	if (ret != 0) {
+		printk("Error %d: failed to configure interrupt on %s pin %d\n",
+			ret, button.port->name, button.pin);
+		return 0;
+	}
+
+
+
+	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
+	gpio_add_callback(button.port, &button_cb_data);
+	printk("Set up button at %s pin %d\n", button.port->name, button.pin);
+	//Switch 2nd LED state so they alternatively blink
 	gpio_pin_toggle_dt(&led2);
 
 	while (1) {
@@ -49,6 +93,13 @@ int main(void)
 		ret = gpio_pin_toggle_dt(&led2);
 		if (ret < 0) {
 			return 0;
+		}
+
+		int val = gpio_pin_get_dt(&button);
+
+		if (val > 0) {
+			//gpio_pin_set_dt(&led, val);
+			gpio_pin_toggle_dt(&led);
 		}
 
 		led_state = !led_state;
